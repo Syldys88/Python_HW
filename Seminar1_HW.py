cc7132dd12c87032468ada33cc77351b442a6f06
Syldys Bulugun <bulugun.syldys@yandex.ru>
2022-09-06 11:14:44 +0300

    Add HW1

---
+++ Seminar1_HW.py
@@ -0,0 +1,12 @@
+def weekend(a):
+  a = a % 7
+  if ((a==0)or(a==6)):
+    return 'Да'
+  return 'Нет'
+
+try:
+  enter = int(input('Введите целое число - '))
+except:
+  print('Не корректное значение')
+
+print (f'День № {enter} выходной? - {weekend(enter)}')
