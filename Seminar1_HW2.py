fbc1cd4b10036ecce511f8c63f50a37978e182e2
Syldys Bulugun <bulugun.syldys@yandex.ru>
2022-09-06 11:17:10 +0300

    Add HW2

---
+++ Seminar1_HW2.py
@@ -0,0 +1,18 @@
+def test(x,y,z):
+  if not(x or y or z) == ((not(x)) and (not(y)) and (not(z))):
+    return(True)
+  else:
+    return(False)
+  
+#print(test(True,False,False))
+
+marker = True
+for i in False,True:
+  for j in False,True:
+    for k in False,True:
+      if(test(i,j,k)!=True):
+        marker = False
+
+print(marker)
+
+print(test(True,False,True))
